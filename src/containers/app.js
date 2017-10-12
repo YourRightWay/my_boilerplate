ae39152b75cc0d06ca51820a2193e603b13e27be
unknown <ts_eliot@mail.ru>
2017-10-12 23:00:01 +0300

    test

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,15 +1,70 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import * as shapes from 'd3-shape';
+
+export function mathRadians(degrees) {
+    return (degrees * Math.PI) / 180;
+}
+
+function angleCoordinates(centerX, centerY, radius, angleInDegrees) {
+    return {
+        x: centerX + (radius * Math.cos(mathRadians(angleInDegrees - 90))),
+        y: centerY + (radius * Math.sin(mathRadians(angleInDegrees - 90)))
+    };
+}
+
+function generatePathFromD3(outerRadius, innerRadius, startAngle, endAngle, padAngle, padRadius) {
+    return shapes
+        .arc()
+        .innerRadius(innerRadius)
+        .outerRadius(outerRadius)
+        .startAngle(startAngle)
+        .endAngle(endAngle)
+        .padAngle(padAngle)
+        .padRadius(padRadius);
+}
 
 class App extends React.Component {
     render() {
+        const { w, h, r, angle1, angle2 } = this.props;
+        
+        const a1 = {
+            x: angleCoordinates(0, 0, r, angle1).x,
+            y: angleCoordinates(0, 0, r, angle1).y,
+        };
+
+        const a2 = {
+            x: angleCoordinates(0, 0, r, angle2).x,
+            y: angleCoordinates(0, 0, r, angle2).y,
+        };
+        
+        let cos = Math.cos(mathRadians(70));
+        let a = 200 * 200 + 200 * 200;
+        let b = (200 * 200) * 2;
+        
+        let result = Math.sqrt(a - b * cos)
+        console.log(result);
+
         return (
-            <div className="container">
-            
-            </div>
+            <svg width="500" height="500">
+                <g transform={`translate(${w / 2}, ${h / 2})`}>
+                    <circle r={r} cx={0} cy={0} strokeWidth="1" stroke="#000" fill="transparent" />
+                    <line x1={0} y1={0} x2={a1.x} y2={a1.y} strokeWidth="1" stroke="#000" />
+                    <line x1={0} y1={0} x2={a2.x} y2={a2.y} strokeWidth="1" stroke="#000" />
+                    <line x1={a1.x} y1={a1.y} x2={a2.x} y2={a2.y} strokeWidth="1" stroke="red" />
+                </g>
+            </svg>
         );
     }
 }
+
+App.defaultProps = {
+    w: 500,
+    h: 500,
+    r: 200,
+    angle1: 50,
+    angle2: 120
+};
 
 App.propTypes = {
     children: PropTypes.element
--- src/index.js
+++ src/index.js
@@ -1,128 +1,26 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import CSSModules from 'react-css-modules';
-import nanoId from 'nanoid';
-import classNames from 'classnames';
-import { connect } from 'react-redux';
-import Utils from '../../../../utils';
-import { saveId } from '../../../../actions/studies';
-import styles from './study-sector.css';
+import React from 'react';
+import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import App from './containers/app';
+import './styles.scss';
 
-class StudySector extends Component {
-    constructor(props) {
-        super(props);
+require('./favicon.ico');
 
-        this.state = {
-            id: false
-        };
-    }
+render(
+    <AppContainer>
+        <App />
+    </AppContainer>,
+    document.getElementById('root')
+);
 
-    componentWillMount() {
-        if (this.props.currentSectorId === this.props.id) {
-            this.setState(() => Object.assign({
-                id: this.props.currentSectorId
-            }));
-        };
-    }
-
-    renderPath() {
-        const { arcs, milestones } = this.props;
-        return arcs.map((data, index) => (
-            <path
-                key={nanoId(6)}
-                fill={Utils.defineMilestoneColor(milestones[index].status, milestones[index].completion)}
-                d={data} />
-        ));
-    }
-
-    onMouseEnterHandler(id) {
-        this.props.saveId(id);
-    }
-
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.currentSectorId === this.props.id) {
-            this.setState(() => Object.assign({ id: nextProps.currentSectorId }));
-        } else {
-            this.setState(() => Object.assign({ id: false }));
-        }
-    }
-
-    shouldComponentUpdate(nextProps) {
-        return this.state.id === this.props.id || this.props.id === nextProps.currentSectorId;
-    }
-
-    renderIndicator() {
-        const { indX, indY, indRadius, indColor, id, currentSectorId } = this.props;
-        if (id !== currentSectorId) return null;
-
-        return (
-            <g>
-                <circle
-                    className="study-indicator"
-                    stroke={Utils.colors.LIGHT_BLUE}
-                    r={indRadius}
-                    fill={indColor}
-                    cx={indX}
-                    cy={indY} />
-                <circle
-                    r={indRadius + (indRadius / 2)}
-                    fill="transparent"
-                    cx={indX}
-                    cy={indY} />
-            </g>
+if (module.hot) {
+    module.hot.accept('./containers/app', () => {
+        const NewRoot = require('./containers/app').default;
+        render(
+            <AppContainer>
+                <NewRoot />
+            </AppContainer>,
+            document.getElementById('root')
         );
-    }
-
-    render() {
-        const { clickHandler, id, currentSectorId } = this.props;
-
-        const activeClass = classNames({
-            'study-sector study-sector__active': currentSectorId === id,
-            'study-sector study-sector__notactive': currentSectorId !== id,
-            'study-sector': !id,
-        });
-
-        return (
-            <g
-                className={activeClass}
-                onMouseEnter={() => id && this.onMouseEnterHandler(id)}
-                onClick={() => clickHandler && clickHandler()}>
-                {this.renderPath()}
-                {this.renderIndicator()}
-            </g>
-        );
-    }
+    });
 }
-
-StudySector.defaultProps = {
-    clickHandler: null,
-    id: undefined,
-    indColor: '#fff',
-    indRadius: 8,
-};
-
-function mapStateToProps(state) {
-    return {
-        currentSectorId: state.sectors.currentSectorId
-    };
-}
-
-const mapDispatchToProps = {
-    saveId
-};
-
-StudySector.propTypes = {
-    arcs: PropTypes.array.isRequired,
-    clickHandler: PropTypes.func,
-    currentSectorId: PropTypes.string.isRequired,
-    id: PropTypes.string,
-    indColor: PropTypes.string,
-    indRadius: PropTypes.number,
-    indX: PropTypes.number.isRequired,
-    indY: PropTypes.number.isRequired,
-    milestones: PropTypes.array.isRequired
-};
-
-const ConnectedStudySector = CSSModules(StudySector, styles);
-
-export default connect(mapStateToProps, mapDispatchToProps)(ConnectedStudySector);
