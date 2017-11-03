106c2e9d3c2bbd8e5c500a0fab581ed52b6f14b7
unknown <ts_eliot@mail.ru>
2017-11-03 21:49:55 +0300

    callbacks example

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
+function requestUser () {
+    setTimeout(function () {
+        console.log('requestUser: ', 'id:', 123 );
+        const id = 123;
+        requestPayment (id)
+    }, 1200)
+}
 
+function requestPayment (userId) {
+    setTimeout(function () {
+        console.log('requestPayment: ', 'userId:', userId )
+        if (userId) {
+            const payment = {
+                name: 'Privat24',
+                quantity: 2000
+            };
+            requestCalculation(payment);
+        }
+    }, 500)
+}
+
+function requestCalculation (payment) {
+    setTimeout(function () {
+        const calculation = Math.round(Math.sqrt((Math.random() * payment.quantity)));
+        console.log('requestcalculation: ', 'payment name:', payment.name, 'payment calculation:', calculation );
+    }, 2200)
+}
+
+requestUser();
