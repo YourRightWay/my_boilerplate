55edd1860552757b67cddbd1b603eea9dde4343e
unknown <ts_eliot@mail.ru>
2017-11-03 20:22:54 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,18 +1,1 @@
-const data = 123;
-const obj = {
-    [data]:data,
-    str: 'string',
-    aaa: 12345
-};
-
-const test = {
-    [data]:data,
-    str: 'string',
-    aaa: 12345
-};
-
-const lololo = {
-    [data]:data,
-    str: 'string',
-    aaa: 12345
-};
+console.log(123)
