c32802d0fa10f43eb8de800c9aae946e2a754ec6
unknown <ts_eliot@mail.ru>
2017-11-03 23:01:09 +0300

    Add promise example with fake data

--- src/index.js
+++ src/index.js
@@ -1,41 +1,49 @@
-import { callApi } from './utils'
-
-function makeRequest() {
-    return callApi('https://next.json-generator.com/api/json/get/41vKPcSRQ', {
-        method: 'GET'
-    }).then(function (result) {
-        return result;
-    }).catch(function (error) {
-        throw new Error(`'https://lesson-4a0eb.firebaseio.com' ${'\n'} ${error}`);
-    })
+function makeRequest(url) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, false);
+    xhr.send();
+    
+    return xhr;
 }
 
-const promiseTrigger = true;
-function getSomeThink(data) {
-    console.info('getSomeThink', 'data: ', data)
+function getFakeData() {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            const xhr = makeRequest('./data/fakeData.json');
 
-    const promise = new Promise(function (resolve, reject) {
-        if(!promiseTrigger) return reject(new Error('Wrong!!!'));
-        resolve({ test: data });
+            if (xhr.status != 200) {
+                reject(new Error(xhr.status + ': ' + xhr.statusText))
+            } else {
+                resolve(xhr.responseText)
+            }
+        }, 2000)
     });
-    
-    return promise;
 }
 
-function getData (data) {
-    setTimeout(function () {
-        console.log('test', data);
-    }, 4000);
+function checkData(data) {
+    console.log(data);
+    return data
 }
 
-makeRequest()
-    .then(getSomeThink)
-    .then(getData)
-    
-    
-  
+function getUser() {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            const xhr = makeRequest('./data/user.json');
 
-  
+            if (xhr.status != 200) {
+                reject(new Error(xhr.status + ': ' + xhr.statusText))
+            } else {
+                resolve(xhr.responseText)
+            }
+        }, 2000)
+    });
+}
+
+getFakeData()
+    .then(checkData)
+    .then(getUser)
+    .then(checkData)
+    .catch();
 
 
 
